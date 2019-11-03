22bbf218bb7d7a76f2753f7fb83b92411f837707
agaoxu <xy20539@163.com>
2019-11-04 00:35:49 +0800

    maven基本知识学习完了，想要深入，看maven实战这本书。

--- user-dao/src/main/java/App.java
+++ user-dao/src/main/java/App.java
@@ -1,7 +1,11 @@
+
 /**
  * @author gaoxu
  * @date 2019-08-02 10:41
  * @description ... 类
  */
 public class App {
+    public static void main(String[] args) {
+        /*JSONArray jsonArray;*/
+    }
 }
